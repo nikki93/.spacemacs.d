b0412e15a26253f991361da448acff25ea51b3ed
nikki93 <s.nikhilesh@gmail.com>
2017-01-05 23:57:01 -0800

    add 4clojure util

--- nikki93/packages.el
+++ nikki93/packages.el
@@ -5,6 +5,7 @@
         flycheck
         tern
         magit
+        4clojure
         auctex
         glsl-mode
         darkroom
@@ -48,6 +49,43 @@
 
       ;; Mode-line icon
       (spacemacs|diminish paredit-mode " (Ⓟ)" " (P)"))))
+
+(defun nikki93/init-4clojure ()
+  (use-package 4clojure
+    :init
+    (progn
+      (defun nikki93/4clojure-check-and-proceed ()
+        "Check the answer and show the next question if it worked."
+        (interactive)
+        (unless
+            (save-excursion
+              ;; Find last sexp (the answer).
+              (goto-char (point-max))
+              (forward-sexp -1)
+              ;; Check the answer.
+              (cl-letf ((answer
+                         (buffer-substring (point) (point-max)))
+                        ;; Preserve buffer contents, in case you failed.
+                        ((buffer-string)))
+                (goto-char (point-min))
+                (while (search-forward "__" nil t)
+                  (replace-match answer))
+                (string-match "failed." (4clojure-check-answers))))
+          (4clojure-next-question)))
+
+      (defadvice 4clojure/start-new-problem
+          (after nikki93/4clojure/start-new-problem-advice () activate)
+        ;; Prettify the 4clojure buffer.
+        (goto-char (point-min))
+        (forward-line 2)
+        (forward-char 3)
+        (fill-paragraph)
+        ;; Position point for the answer
+        (goto-char (point-max))
+        (insert "\n\n\n")
+        (forward-char -1)
+        ;; Define our key.
+        (local-set-key (kbd "M-j") #'nikki93/4clojure-check-and-proceed)))))
 
 ;; (defun nikki93/post-init-flycheck ()
 ;;   (use-package flycheck
